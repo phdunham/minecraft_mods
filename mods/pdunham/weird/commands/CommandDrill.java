15a65a51f1bcd105d9f4d58cb7e9950be059f15d
rv-pdunham <phdunham2@gmail.com>
2013-05-20 20:51:09 -0700

    Updated drill code to be cleaner. Also added more shapes to the drilling.

--- mods/pdunham/weird/commands/CommandDrill.java
+++ mods/pdunham/weird/commands/CommandDrill.java
@@ -1,5 +1,6 @@
 package pdunham.weird.commands;
 
+import java.util.Arrays;
 import java.util.List;
 
 import pdunham.weird.common.StandardLogger;
@@ -12,7 +13,8 @@ import net.minecraft.world.WorldServer;
 
 public class CommandDrill extends CommandBase {
 	private static StandardLogger logger = new StandardLogger();
-	private String drillMode = "default";
+	private String drillMode = "worm";
+	private String validShapes [] = { "cone", "cube", "hall", "tube", "worm" };
 	
 	public String getDrillMode() {
 		return drillMode;
@@ -33,26 +35,23 @@ public class CommandDrill extends CommandBase {
 
     public void processCommand(ICommandSender par1ICommandSender, String[] par2ArrayOfStr) {
     		logger.info(" processCommand " + par1ICommandSender + " : " + par2ArrayOfStr);
+    		
+    		// The drill offers a variety of shapes for the user to chose from. Store the
+    		// shape selected in the drillMode variable.
         if (par2ArrayOfStr.length == 1) {
-        		String arg = par2ArrayOfStr[0]; 
+        		String arg = par2ArrayOfStr[0].toLowerCase();
 
-        		if (arg.equals("default")) {
-        			drillMode = "default";
-                notifyAdmins(par1ICommandSender, "commands.drill.default", new Object[] { "default" });
+        		if (Arrays.asList(validShapes).indexOf(arg) >= 0) {
+        			drillMode = arg;
+                notifyAdmins(par1ICommandSender, "commands.drill." + arg, new Object[] { arg });
                 return;
-            }
-
-            if (arg.equals("tube")) {
-            		drillMode = "tube";
-                notifyAdmins(par1ICommandSender, "commands.drill.tube", new Object[] { "tube" });
-                return;
-            }
+        		}
         }
         throw new WrongUsageException("commands.drill.usage", new Object[0]);
     }
 
     // Adds the strings available in this command to the given list of tab completion options.
     public List addTabCompletionOptions(ICommandSender par1ICommandSender, String[] par2ArrayOfStr) {
-        return getListOfStringsMatchingLastWord(par2ArrayOfStr, new String[] {"default", "tube"});
+        return getListOfStringsMatchingLastWord(par2ArrayOfStr, validShapes);
     }
 }
--- mods/pdunham/weird/tools/WeirdWormDrill.java
+++ mods/pdunham/weird/tools/WeirdWormDrill.java
@@ -24,13 +24,16 @@ public class WeirdWormDrill extends ItemPickaxe {
 
 	private static StandardLogger logger = new StandardLogger();
 	private static List<Integer> nonHarvestableBlocks = new ArrayList<Integer>();
+	private int areaDrilledCount = 0;
+	private int blockHarvestCount = 0;
+
 
  	// Standard c'tor
 	public WeirdWormDrill(int id) {
         super(id, EnumToolMaterial.EMERALD);
 
-        // Limit the stack size to a weird number
-        setMaxStackSize(2);
+        // Limit the stack size
+        setMaxStackSize(1);
         
         // Put on the materials tab
         setCreativeTab(CreativeTabs.tabTools);
@@ -80,7 +83,8 @@ public class WeirdWormDrill extends ItemPickaxe {
 		return true;
 	}
 	
-	private boolean harvest(EntityPlayer player, World world, int x, int y, int z) {
+	// Attempt to harvest the block at the coordinates. 
+	private boolean harvestBlock(EntityPlayer player, World world, int x, int y, int z) {
 		// Check to see if there is a block at the location
 		boolean harvested = false;
 		int blockID = world.getBlockId(x,  y,  z);
@@ -94,152 +98,154 @@ public class WeirdWormDrill extends ItemPickaxe {
 				Block block = Block.blocksList[blockID];
 				block.dropBlockAsItem(world, x, y, z, meta, 1);
 				// logger.info("Harvested " + block.getBlockName());
+				blockHarvestCount++;
 				harvested = true;
 			}
 			// Remove the block at the location
 			world.setBlockAndMetadata(x, y, z, 0, 0);
 		}
+		areaDrilledCount++;
 		return harvested;
 	}
-	private void activateWormDrill(EntityPlayer player, World world, int xs, int ys, int zs, int blockFace) {
-		int delta = 2;
-		int areaDrilledCount = 0;
-		int blockHarvestCount = 0;
-		for (int d1 = -delta; d1 <= delta; d1++) {
-			for (int d3 = Math.abs(d1) - delta; d3 <= delta - Math.abs(d1); d3++) {
-				for (int d2 = Math.abs(d1) - delta; d2 <= delta - Math.abs(d1); d2++) {
- 					int x = 0, y = 0, z = 0;
+	
+	// Take the original coords, combine them with the deltas and the direction we are facing (blockFace), 
+	// normalize them and harvest the block at the normalized coordinates
+	private boolean harvestBlockWithOffset(EntityPlayer player, World world, int xs, int ys, int zs, int blockFace, int d1, int d2, int d3, int delta) {
+			int x = 0, y = 0, z = 0;
+				switch (blockFace) {
+				// Block faces:
+				// +y direction
+	            case 0:
+ 					x = xs + d2;
+ 					y = ys + d3 + delta;
+ 					z = zs + d1;
+                break;
+				
+                // -y direction
+	            case 1:
+ 					x = xs + d2;
+ 					y = ys - d3 - delta;
+ 					z = zs + d1;
+                break;
+				
+                // +z direction
+	            case 2:
+ 					x = xs + d1;
+ 					y = ys + d2 + delta;
+ 					z = zs + d3 + delta;
+                break;
+			
+                // -z direction
+	            case 3:
+ 					x = xs + d1;
+ 					y = ys + d2 + delta;
+ 					z = zs - d3 - delta;
+                break;
+					
+                // +x direction
+	            case 4:
+ 					x = xs + d3 + delta;
+ 					y = ys + d2 + delta;
+ 					z = zs + d1;
+                break;
 
- 					switch (blockFace) {
- 						// Block faces:
-						//   y = 0
-			            case 0:
-		 					z = zs + d3;
-		 					y = ys + d1 + delta;
-		 					x = xs + d2;
-	                    break;
-	 				
-	                    //  -y = 1
-			            case 1:
-		 					z = zs - d3;
-		 					y = ys + d1 - delta;
-		 					x = xs + d2;
-	                    break;
-	 				
-	                    //   z = 2
-			            case 2:
-		 					z = zs + d3 + delta;
-		 					x = xs + d1;
-		 					y = ys + d2 + delta;
-	                    break;
-	 			
-	                    //  -z = 3
-			            case 3:
-		 					z = zs - d3 - delta;
-		 					x = xs + d1;
-		 					y = ys + d2 + delta;
-	                    break;
-	 					
-		                //   x = 4
-			            case 4:
-		 					x = xs + d3 + delta;
-		 					z = zs + d1;
-		 					y = ys + d2 + delta;
-		                break;
-
-		                //  -x = 5
-			            case 5:
-		 					x = xs - d3 - delta;
-		 					z = zs + d1;
-		 					y = ys + d2 + delta;
-	                    break;
-					}
-					if (harvest(player, world, x, y, z)) {
-						blockHarvestCount++;
-					}
-					areaDrilledCount++;
+                // -x direction
+	            case 5:
+ 					x = xs - d3 - delta;
+ 					y = ys + d2 + delta;
+ 					z = zs + d1;
+                break;
+		}
+		return harvestBlock(player, world, x, y, z);
+	}
+	
+	// Harvest a sorta sphere shaped area
+	private void activateWormDrill(EntityPlayer player, World world, int x, int y, int z, int blockFace) {
+		int width = 2;
+		for (int d1 = -width; d1 <= width; d1++) {
+			for (int d2 = Math.abs(d1) - width; d2 <= width - Math.abs(d1); d2++) {
+				for (int d3 = Math.abs(d1) - width; d3 <= width - Math.abs(d1); d3++) {
+					harvestBlockWithOffset(player, world, x, y, z, blockFace, d1, d2, d3, width);
 				}
 			}
 		}
-		// create a circular hole. Set the direction based on which block face is clicked on.
-		logger.info("Activated " + xs + ", " + ys + ", " + zs + ", " + blockFace + " area harvested: " + areaDrilledCount + " blocks reclaimed " + blockHarvestCount);
 	}
 
-	private void activateTubeDrill(EntityPlayer player, World world, int xs, int ys, int zs, int blockFace) {
-		int delta = 1;
-		int areaDrilledCount = 0;
-		int blockHarvestCount = 0;
-		for (int d1 = -delta; d1 <= delta; d1++) {
-				for (int d2 = 0; d2 <= (delta * 2) + 1; d2++) {
-					for (int d3 = 0; d3 <= 15; d3++) {
- 					int x = 0, y = 0, z = 0;
-
- 					switch (blockFace) {
- 						// Block faces:
-						//   y = 0
-			            case 0:
-		 					x = xs + d1;
-		 					y = ys + d3;
-		 					z = zs + d2;
-	                    break;
-	 				
-	                    //  -y = 1
-			            case 1:
-		 					x = xs + d1;
-		 					y = ys - d3;
-		 					z = zs + d2;
-	                    break;
-	 				
-	                    //   z = 2
-			            case 2:
-		 					x = xs + d1;
-		 					y = ys + d2;
-		 					z = zs + d3;
-	                    break;
-	 			
-	                    //  -z = 3
-			            case 3:
-		 					x = xs + d1;
-		 					y = ys + d2;
-		 					z = zs - d3;
-	                    break;
-	 					
-		                //   x = 4
-			            case 4:
-		 					x = xs + d3;
-		 					y = ys + d2;
-		 					z = zs + d1;
-		                break;
-
-		                //  -x = 5
-			            case 5:
-		 					x = xs - d3;
-		 					y = ys + d2;
-		 					z = zs + d1;
-	                    break;
-					}
-					if (harvest(player, world, x, y, z)) {
-						blockHarvestCount++;
-					}
-					areaDrilledCount++;
+	// Harvest a rectangular tube area
+	private void activateTubeDrill(EntityPlayer player, World world, int x, int y, int z, int blockFace) {
+		int size = 1;
+		for (int width = -size; width <= size; width++) {
+			for (int height = 0; height <= (size * 2) + 1; height++) {
+				for (int depth = 0; depth <= 15; depth++) {
+					harvestBlockWithOffset(player, world, x, y, z, blockFace, width, height, depth, 0);
 				}
 			}
 		}
-		// create a circular hole. Set the direction based on which block face is clicked on.
-		logger.info("Activated " + xs + ", " + ys + ", " + zs + ", " + blockFace + " area harvested: " + areaDrilledCount + " blocks reclaimed " + blockHarvestCount);
+	}
+
+	// Harvest a long thin hallway area
+	private void activateHallDrill(EntityPlayer player, World world, int x, int y, int z, int blockFace) {
+		int d1 = 0;
+		for (int d2 = 0; d2 < 2; d2++) {
+			for (int d3 = 0; d3 < 45; d3++) {
+				harvestBlockWithOffset(player, world, x, y, z, blockFace, d1, d2, d3, 0);
+			}
+		}
+	}
+	
+	// Harvest a large cube area
+	private void activateCubeDrill(EntityPlayer player, World world, int x, int y, int z, int blockFace) {
+		int width = 2;
+		for (int d1 = -width; d1 <= width; d1++) {
+			for (int d2 = 0; d2 <= (width * 2) + 1; d2++) {
+				for (int d3 = 0; d3 <= (width * 2) + 1; d3++) {
+					harvestBlockWithOffset(player, world, x, y, z, blockFace, d1, d2, d3, 0);
+				}
+			}
+		}
+	}
+
+	// Harvest a large cube area
+	private void activateConeDrill(EntityPlayer player, World world, int x, int y, int z, int blockFace) {
+		int width = 1;
+		for (int d3 = 0; d3 <= (width * 2) + 1; d3++) {
+			for (int d1 = -d3; d1 <= d3; d1++) {
+				for (int d2 = -d3 - 1; d2 <= d3 + 1; d2++) {
+					harvestBlockWithOffset(player, world, x, y, z, blockFace, d1, d2, d3, 0);
+				}
+			}
+		}
 	}
 	
 	@Override
+	// When the drill is right clicked on a block, select the harvest shape and harvest it.
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int blockFace, float par8, float par9, float par10) {
-		if (!world.isRemote) {
-			// logger.info("onItemUse location " + x + ", " + z + "  h=" + y + " player=" + player);
-			if (WeirdMain.commandDrill.getDrillMode() == "tube") {
-				activateTubeDrill(player, world, x, y, z, blockFace);
-				return true;
-			}
-			activateWormDrill(player, world, x, y, z, blockFace);
-			return true;
+		
+		// Only works on the !remote side
+		if (world.isRemote) {
+			return false;
 		}
-		return false;
+
+		String mode = WeirdMain.commandDrill.getDrillMode();
+		logger.info("onItemUse location " + x + ", " + z + "  h=" + y + " mode='" + mode + "'");
+		
+		// Reset the statistics
+		areaDrilledCount = 0;
+		blockHarvestCount = 0;
+
+		if (mode.equals("cone")) {
+			activateConeDrill(player, world, x, y, z, blockFace);
+		} else if (mode.equals("cube")) {
+			activateCubeDrill(player, world, x, y, z, blockFace);
+		} else if (mode.equals("hall")) {
+			activateHallDrill(player, world, x, y, z, blockFace);
+		} else if (mode.equals("tube")) {
+			activateTubeDrill(player, world, x, y, z, blockFace);
+		} else {
+			// 'worm' is the default
+			activateWormDrill(player, world, x, y, z, blockFace);
+		} 
+		logger.info(mode + " mode " + x + ", " + y + ", " + z + ", " + blockFace + " area harvested: " + areaDrilledCount + " blocks reclaimed " + blockHarvestCount);
+		return true;
 	}
 }
